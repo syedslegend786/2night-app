5e16ffe34661f19dbf8ea29bdf74a138f54651d0
Syed Salleh M.Shah <syedslegend786@gmail.com>
2023-05-13 01:02:40 +0500

    fixing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import Head from 'next/head'
 export default function App({ Component, pageProps: { session, ...props } }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
       <main>
         <Component {...props} />
       </main>
