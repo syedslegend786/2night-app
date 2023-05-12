e37aa0d7e361d81cd54368158cd5214b46163e50
Syed Salleh M.Shah <syedslegend786@gmail.com>
2023-05-13 00:41:21 +0500

    adding dvh instead vh

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  
   return (
     <Html lang="en">
-      <Head >
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </Head>
+      <Head />
+
       <body>
         <Main />
         <NextScript />
