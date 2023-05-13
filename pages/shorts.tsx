9c99edcf11a99d43778477e1456fd666f8766877
Syed Salleh M.Shah <syedslegend786@gmail.com>
2023-05-13 14:08:26 +0500

    trying fixing

--- pages/shorts.tsx
+++ pages/shorts.tsx
@@ -8,24 +8,24 @@ import { useRouter } from 'next/router'
 const ShortsPage = () => {
     const router = useRouter()
     return (
-        <div className='relative screen_height w-full'>
-            <div onClick={() => { router.push("/")}} className='absolute top-[20px] left-[20px] z-50 flex items-center gap-x-2'>
+        <div className='relative h-screen max-h-screen w-full'>
+            <div onClick={() => { router.push("/") }} className='absolute top-[20px] left-[20px] z-50 flex items-center gap-x-2'>
                 <HiArrowLeft className='text-white' fontSize={24} />
                 <div className='w-[83px] h-[20px] relative'>
                     <Image src={"/logo.png"} alt='' fill className='object-contain' />
                 </div>
             </div>
-            <div className='screen_height  w-full snap-y overflow-scroll snap-mandatory'>
-                <div className='snap-center'>
+            <div className='h-screen max-h-screen  w-full snap-y overflow-scroll snap-mandatory'>
+                <div className='snap-center h-full max-h-full overflow-hidden'>
                     <Short />
                 </div>
-                <div className='snap-center'>
+                <div className='snap-center h-full max-h-full '>
                     <Short />
                 </div>
-                <div className='snap-center'>
+                <div className='snap-center h-full max-h-full '>
                     <Short />
                 </div>
-                <div className='snap-center'>
+                <div className='snap-center h-full max-h-full '>
                     <Short />
                 </div>
             </div>
